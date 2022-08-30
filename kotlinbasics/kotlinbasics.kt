27feee8bf29eebfc44f1925605f69348b5b4c6ca
TimzOwen <timzowen@gmail.com>
2022-08-30 14:21:58 +0300

    .variavlebs

--- kotlinbasics/kotlinbasics.kt
+++ kotlinbasics/kotlinbasics.kt
@@ -1,6 +1,19 @@
 // Main function that prints hello world
-fun main() {
-    println("Hello, world!")
+fun main(){
+    print("Hello Android !")
 }
 
+
+
+// print more than one line
+fun main(){
+    println("Hey line one !")
+    println("Hello line 2 ! ")
+}
+
+// Fixing Errors, 
+
+/* While writing code you will face some errors ,
+In android studion this can be seen on the log cat and marked with 
+Red color if one is using other editors */
 
