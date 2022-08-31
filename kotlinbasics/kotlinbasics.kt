511027c0f73cc73c0bb6fb2ac0cdff27688e8f4e
TimzOwen <timzowen@gmail.com>
2022-08-31 11:30:03 +0300

    functions update

--- kotlinbasics/kotlinbasics.kt
+++ kotlinbasics/kotlinbasics.kt
@@ -17,3 +17,18 @@ fun main(){
 In android studion this can be seen on the log cat and marked with 
 Red color if one is using other editors */
 
+// Kotlin Variables
+// used to store values temporarily in comp space.-->booking space
+// Integers,strings, double, float
+fun main(){
+    val name : String = "Timz"
+    val age : Int = 15
+    val bankBal : Double = 15000.50
+
+    println(name)
+    println(age)
+    println(bankBal)
+}
+
+
+
