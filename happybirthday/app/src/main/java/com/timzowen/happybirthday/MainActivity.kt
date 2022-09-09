c921a048ebb68bf66079d6c49096bdd08e13f40d
TimzOwen <timzowen@gmail.com>
2022-09-09 20:53:18 +0300

    row alignent

--- happybirthday/app/src/main/java/com/timzowen/happybirthday/MainActivity.kt
+++ happybirthday/app/src/main/java/com/timzowen/happybirthday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.timzowen.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.timzowen.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,24 +31,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayCardText(message : String){
-
-}
-
-@Composable
-fun BackButton(){
-
-}
-
-@Composable
-fun DrawTextField(){
-
+fun BirthdayCardText(message : String, from : String){
+    Row {
+        Text(text = message, fontSize = 26.sp)
+        Text(text = from, fontSize = 24.sp)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HappyBirthdayTheme {
+        BirthdayCardText(message = "Happy Birthday Sam!", from = "From Timz Owen")
 
     }
 }
