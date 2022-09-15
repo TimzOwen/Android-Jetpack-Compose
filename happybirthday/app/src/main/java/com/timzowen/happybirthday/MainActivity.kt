f7b18302ca6a5a16ccb712769e34c4d0a93cfab2
TimzOwen <timzowen@gmail.com>
2022-09-15 18:52:33 +0300

    ,

--- happybirthday/app/src/main/java/com/timzowen/happybirthday/MainActivity.kt
+++ happybirthday/app/src/main/java/com/timzowen/happybirthday/MainActivity.kt
@@ -4,18 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.timzowen.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -52,8 +51,14 @@ fun BirthDayWithImage(message: String, from: String){
 @Composable
 fun BirthdayCardText(message : String, from : String){
     Column() {
-        Text(text = message, fontSize = 18.sp)
-        Text(text = from, fontSize = 16.sp)
+        Text(text = message, fontSize = 32.sp, modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally)
+            .padding(start = 16.dp, top = 16.dp))
+        Text(text = from, fontSize = 26.sp, modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally)
+            .padding(start = 16.dp, end = 16.dp))
     }
 }
 
