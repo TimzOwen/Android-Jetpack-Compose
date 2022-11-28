a9ab3e1520244469b60675cdb636ab760f731b57
TimzOwen <timzowen@gmail.com>
2022-11-28 22:11:24 +0300

    understanding column and Row in compose

--- happybirthday/app/src/main/java/com/timzowen/happybirthday/MainActivity.kt
+++ happybirthday/app/src/main/java/com/timzowen/happybirthday/MainActivity.kt
@@ -3,18 +3,13 @@ package com.timzowen.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.timzowen.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -27,7 +22,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colors.background
                 ) {
-                    BirthDayWithImage(message = "Hello Compose!", from = "From Timz Owen")
+
                 }
             }
         }
@@ -35,37 +30,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthDayWithImage(message: String, from: String){
-    val image = painterResource(id = (R.drawable.androidparty))
-    Box{
-       Image(painter = image, contentDescription = null,
-           modifier = Modifier
-               .fillMaxHeight()
-               .fillMaxWidth(),
-           contentScale = ContentScale.Crop
-       )
-        BirthdayCardText(message = message, from =from)
+fun BirthDayCardWithText(message : String, from: String){
+    Column{
+         Text(
+             text = message,
+            fontSize = 34.sp
+         )
+        Text(
+            text = from,
+            fontSize = 24.sp
+        )
     }
 }
 
+@Preview(showBackground = false)
 @Composable
-fun BirthdayCardText(message : String, from : String){
-    Column() {
-        Text(text = message, fontSize = 32.sp, modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentWidth(Alignment.CenterHorizontally)
-            .padding(start = 16.dp, top = 16.dp))
-        Text(text = from, fontSize = 26.sp, modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentWidth(Alignment.CenterHorizontally)
-            .padding(start = 16.dp, end = 16.dp))
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
+fun BirthDayCardPreview(){
     HappyBirthdayTheme {
-        BirthDayWithImage(message = "Hey Jetpack!", from = "From Timz Owen")
+        BirthDayCardWithText(message = "Welcome to Compose ","From Owen")
     }
 }
+
+
