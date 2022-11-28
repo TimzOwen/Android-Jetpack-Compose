ade29a62ff901e87bd594436bd7b319b8ec90318
TimzOwen <timzowen@gmail.com>
2022-11-28 23:25:49 +0300

    composables positioning

--- happybirthday/app/src/main/java/com/timzowen/happybirthday/MainActivity.kt
+++ happybirthday/app/src/main/java/com/timzowen/happybirthday/MainActivity.kt
@@ -4,17 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.timzowen.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -58,27 +59,26 @@ fun BirthDayCardWithText(message : String, from: String){
     Column{
          Text(
              text = message,
-             fontSize = 34.sp
+             fontSize = 34.sp,
+             modifier = Modifier
+                 .fillMaxWidth()
+                 .wrapContentWidth(Alignment.CenterHorizontally)
+                 .padding(start = 16.dp, end = 16.dp)
          )
         Text(
             text = from,
-            fontSize = 24.sp
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp)
         )
     }
 }
 
-//@Preview(showBackground = false)
-//@Composable
-//fun BirthdayCardImagePreview(){
-//    BirthDayGreetingWithImage("hello Compose"," -From Timz ")
-//}
-
-//@Preview(showBackground = false)
-//@Composable
-//fun BirthDayCardPreview(){
-//    HappyBirthdayTheme {
-//        BirthDayCardWithText(message = "Welcome to Compose ","From Owen")
-//    }
-//}
-
+@Preview
+@Composable
+fun BirthdayGreetingWithImagePreview(){
+    BirthDayGreetingWithImage(message = stringResource(R.string.message), from = stringResource(R.string.from))
+}
 
