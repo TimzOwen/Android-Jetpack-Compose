442a20ddfab684c2338877a3c7781db4fb70d8c4
TimzOwen <timzowen@gmail.com>
2024-06-11 12:56:10 +0300

    padding compose

--- uatRelease01/app/src/main/java/com/timzowen/uat/MainActivity.kt
+++ uatRelease01/app/src/main/java/com/timzowen/uat/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.timzowen.uat.ui.theme.UatTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,14 +20,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            UatTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Greeting(" world")
         }
     }
 }
@@ -35,6 +30,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+            .width(200.dp)
+            .height(250.dp)
+            .clickable {  }
+            .padding(start = 24.dp)
     )
 }
 
@@ -42,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     UatTheme {
-        Greeting("Android")
+        Greeting("World")
     }
 }
