766e0e09be5c9fa1938258f915a0405c1d772613
TimzOwen <timzowen@gmail.com>
2024-06-12 11:24:50 +0300

    Reusable components

--- dynamiccontent/app/src/main/java/com/timzowen/dynamiccontent/MainActivity.kt
+++ dynamiccontent/app/src/main/java/com/timzowen/dynamiccontent/MainActivity.kt
@@ -4,44 +4,52 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.timzowen.dynamiccontent.ui.theme.DynamicContentTheme
+
+private val namesList: ArrayList<String> = arrayListOf("Timz", "Owen", "Shem", "John", "Kiptoo")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DynamicContentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            GreetingList(names = namesList)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingList(names: ArrayList<String>) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+        Button(onClick = { namesList.add("New Name") }) {
+            Text(text = "Add Name")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name", style = MaterialTheme.typography.bodyLarge)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    DynamicContentTheme {
-        Greeting("Android")
-    }
+    GreetingList(names = namesList)
 }
