850aedb9f22235d179faa437994facbf2763eda5
TimzOwen <timzowen@gmail.com>
2024-06-19 20:11:48 +0300

    composition

--- profilelayout/app/src/main/java/com/timzowen/profilelayout/MainActivity.kt
+++ profilelayout/app/src/main/java/com/timzowen/profilelayout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.timzowen.profilelayout
 
+import android.graphics.Paint.Style
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,9 +19,12 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Composition
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,6 +32,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.ContentAlpha
+import androidx.wear.compose.material.LocalContentAlpha
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -88,7 +95,14 @@ fun ProfilePicture() {
 
 @Composable
 fun ProfileContent() {
-    Text(text = "Timz Owen")
+    Column(modifier = Modifier.padding(8.dp)
+        .fillMaxWidth()) {
+        Text(text = "Timz Owen", style = MaterialTheme.typography.headlineSmall)
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(text = "Active now", style = MaterialTheme.typography.bodySmall)
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
