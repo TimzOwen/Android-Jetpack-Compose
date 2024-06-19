7993ee075e33c1eaa16fa7510c4010ae24061c68
TimzOwen <timzowen@gmail.com>
2024-06-19 19:47:10 +0300

    profile card updates

--- profilelayout/app/src/main/java/com/timzowen/profilelayout/MainActivity.kt
+++ profilelayout/app/src/main/java/com/timzowen/profilelayout/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.timzowen.profilelayout
 
-import android.graphics.Paint.Style
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,36 +40,55 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    Column(
+    Surface(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally
+        color = Color.LightGray
     ) {
-        TextViewPreview("Profile")
-        TextViewPreview("Active")
-        TextViewPreview("Last Seen")
-        TextViewPreview("Status")
+        ProfileCard()
+    }
+}
 
-        UpdateProfile()
-
-        Button(onClick = {}) {
-            Text("change profile")
+@Composable
+fun ProfileCard() {
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            ProfilePicture()
+            ProfileContent()
         }
+
     }
 }
 
 @Composable
-fun TextViewPreview(name: String) {
-    Text(text = name, style = MaterialTheme.typography.bodyLarge)
+fun ProfilePicture() {
+    Card(
+        shape = CircleShape,
+        modifier = Modifier.padding(16.dp),
+        border = BorderStroke(width = 2.dp, color = Color.Green),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.profile_one),
+            contentDescription = "Profile Picture",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(75.dp)
+        )
+    }
 }
 
 @Composable
-fun UpdateProfile(){
-    val currentStateUser = arrayListOf("location","item","price","quantity")
-
-    for (currentUser in currentStateUser){
-        Text(text = currentUser, style = MaterialTheme.typography.bodyLarge)
-    }
+fun ProfileContent() {
+    Text(text = "Timz Owen")
 }
 
 @Preview(showBackground = true)
