834fd40f104a8ce5fc860a95e361f35cd6c8433e
TimzOwen <timzowen@gmail.com>
2024-07-05 12:51:54 +0300

    when from if

--- kotlin programming/CodeReview/src/Functions.kt
+++ kotlin programming/CodeReview/src/Functions.kt
@@ -2,10 +2,18 @@
 
 fun main(){
     // call the function here
-    makeCoffee(3)
+    makeCoffee(1, "Owen")
+    makeCoffee(0, "Abdul")
+    makeCoffee(3,"Timz")
+
+
 }
 
 // create functions to be reused
-fun makeCoffee(sugarCount: Int){
-    println("I want $sugarCount  tea spoons")
+fun makeCoffee(sugarCount: Int, userName: String){
+    when(sugarCount){
+        0 -> println("Coffee with no sugar for $userName")
+        1 -> println("Coffee with $sugarCount tea spoon for $userName")
+        else -> println("Coffee with $sugarCount tea spoons for $userName")
+    }
 }
