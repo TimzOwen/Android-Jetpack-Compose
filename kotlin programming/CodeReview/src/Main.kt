ffcf59f4dfec5dddc16ad5db6bdbbb3bf8cb433b
TimzOwen <timzowen@gmail.com>
2024-07-06 11:54:46 +0300

    data classes

--- kotlin programming/CodeReview/src/Main.kt
+++ kotlin programming/CodeReview/src/Main.kt
@@ -1,13 +1,30 @@
-fun main(){
-    println("Enter num 1: ")
-    val num1 = readln().toInt()
-    println("Enter num2: ")
-    val num2 = readln().toInt()
+import classes.Dog
 
-    println("The sum of $num1 + $num2 = " + addNumbers(num1,num2))
+// use data classes to simplify files not needed or called else where.
+data class CoffeeDetails(
+    val sugarCount: Int,
+    val userName: String,
+    val size: String,
+    val creamAmount: Int
+)
 
+fun main() {
+
+    val coffeeList = CoffeeDetails(2,"Timz","XL",4)
+    makeCoffee(coffeeList)
 }
 
-fun addNumbers(num1: Int, num2: Int): Int{
-    return num1 + num2
+fun askCoffeeDetails(){
+    println("whose coffee is it for? ...")
+    val userName = readln()
+    println("How many pieces of sugar?....")
+    val sugarCount = readln().toInt()
+}
+
+fun makeCoffee(coffeeDetails: CoffeeDetails){
+    when(coffeeDetails.sugarCount){
+        0 -> println("Coffee with no sugar for ${coffeeDetails.userName} add cream: ${coffeeDetails.creamAmount} size ${coffeeDetails.size}")
+        1 -> println("Coffee with ${coffeeDetails.sugarCount} spoon for ${coffeeDetails.userName} add cream: ${coffeeDetails.creamAmount} size ${coffeeDetails.size}")
+        else -> println("Coffee with ${coffeeDetails.sugarCount} spoons for ${coffeeDetails.userName} add cream: ${coffeeDetails.creamAmount} size ${coffeeDetails.size}")
+    }
 }
