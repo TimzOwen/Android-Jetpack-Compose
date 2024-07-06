ee84b69209ad90b97600353e28c8d82e72ca4b0a
TimzOwen <timzowen@gmail.com>
2024-07-06 18:44:29 +0300

    dropdown items

--- android_prac_apps/unit_converter/app/src/main/java/com/timzowen/unitconverter/MainActivity.kt
+++ android_prac_apps/unit_converter/app/src/main/java/com/timzowen/unitconverter/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.timzowen.unitconverter
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,10 +8,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -22,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.timzowen.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +36,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UnitConverterTheme {
-                Surface (modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     UnitConverter()
                 }
             }
@@ -41,24 +49,42 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun UnitConverter(){
-    Column(modifier = Modifier.fillMaxSize(),
+fun UnitConverter() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(text = "Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "Enter Values", onValueChange = {})
+        Spacer(modifier = Modifier.padding(16.dp))
         Row {
             Box {
                 Button(onClick = { /*TODO*/ }) {
                     Text(text = "select")
-                    Icon(Icons.Default.ArrowDropDown,  contentDescription = "dropdown")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "dropdown")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem({ Text(text = "Centimeter") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Millimeters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Inches") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Feet") }, onClick = { /*TODO*/ })
                 }
             }
-
+            Spacer(modifier = Modifier.width(16.dp))
             Box {
                 Button(onClick = { /*TODO*/ }) {
                     Text(text = "select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "select")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem({ Text(text = "Centimeter") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Millimeters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Inches") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Feet") }, onClick = { /*TODO*/ })
                 }
             }
         }
@@ -70,5 +96,5 @@ fun UnitConverter(){
 @Preview(showBackground = true)
 @Composable
 fun UnitConverterPreview() {
-        UnitConverter()
+    UnitConverter()
 }
