5485a0cbc5f5513c4b02882dce025dd68ba9cb4f
TimzOwen <timzowen@gmail.com>
2024-07-09 08:19:58 +0300

    shopping items list app

--- android_prac_apps/shopping_list/app/src/main/java/com/timzowen/shopping_list/MainActivity.kt
+++ android_prac_apps/shopping_list/app/src/main/java/com/timzowen/shopping_list/MainActivity.kt
@@ -8,16 +8,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.timzowen.shopping_list.ui.theme.Shopping_listTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,23 +30,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Shopping_listTheme {
-               Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                   Column(modifier = Modifier.fillMaxSize(),
-                       verticalArrangement = Arrangement.Center) {
-                       Button(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
-                           Text(text = "Add Item")
-                       }
-                   }
-               }
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    ShoppingListApp()
+                }
             }
         }
     }
-}
 
-@Composable
-@Preview
-fun ShoppingListPreview() {
-    Shopping_listTheme {
-        
-    }
 }
