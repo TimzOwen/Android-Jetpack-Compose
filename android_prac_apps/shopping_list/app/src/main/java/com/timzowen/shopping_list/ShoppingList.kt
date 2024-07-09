527b19f121805f2a791df60ba65c30dec3228f1f
TimzOwen <timzowen@gmail.com>
2024-07-09 12:28:38 +0300

    aligments

--- android_prac_apps/shopping_list/app/src/main/java/com/timzowen/shopping_list/ShoppingList.kt
+++ android_prac_apps/shopping_list/app/src/main/java/com/timzowen/shopping_list/ShoppingList.kt
@@ -1,14 +1,24 @@
 package com.timzowen.shopping_list
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 
@@ -52,6 +63,7 @@ fun ShoppingListApp() {
                 .padding(16.dp)
         ) {
             items(shoppingItemsList.size) {
+                ShoppingListItem(item = shoppingItemsList[it],{},{})
             }
         }
     }
@@ -105,5 +117,25 @@ fun ShoppingListApp() {
                 }
             }
         )
+    }
+}
+
+@Composable
+fun ShoppingListItem(item: ShoppingItems, onEditClick: () -> Unit, onDeleteClick: () -> Unit){
+    Row (modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth()
+        .border(
+            border = BorderStroke(2.dp, color = Color.Blue),
+            shape = RoundedCornerShape(20)
+        ),
+        horizontalArrangement = Arrangement.SpaceBetween){
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
+        Text(text = "Qty: ${item.quantity} ", modifier = Modifier.padding(8.dp))
+        Row (modifier = Modifier.padding(8.dp)) {
+            Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit button")
+            Spacer(modifier = Modifier.width(16.dp))
+            Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete button")
+        }
     }
 }
