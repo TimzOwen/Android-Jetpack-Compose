9a4cf87b3faf8302629f171ddde8f2c58c05f514
TimzOwen <timzowen@gmail.com>
2024-07-11 12:50:41 +0300

    count function impl

--- android_prac_apps/counterMVVM/app/src/main/java/com/timzowen/countermvvm/MainActivity.kt
+++ android_prac_apps/counterMVVM/app/src/main/java/com/timzowen/countermvvm/MainActivity.kt
@@ -4,14 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +41,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TheCounterScreen() {
-
     val counter = remember { mutableStateOf(0) }
 
     fun increment(){
@@ -39,11 +51,33 @@ fun TheCounterScreen() {
         counter.value --
     }
 
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+
+        Text(text = "Counter: ${counter.value}",
+            fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp))
+        
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row() {
+            Button(onClick = { increment() }) {
+                Text(text = " Increment ")
+            }
+            Spacer(modifier = Modifier.padding(16.dp))
+            Button(onClick = { decrement() }) {
+                Text(text = " Decrement ")
+            }
+        }
+    }
+
 }
 
 
 @Preview(showBackground = true)
 @Composable
 fun TheCounterScreenPreview() {
-
+    TheCounterScreen()
 }
