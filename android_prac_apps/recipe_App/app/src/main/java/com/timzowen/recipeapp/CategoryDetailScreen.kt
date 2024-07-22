1299c443174cc954e907d9d72afa931f5a37dc99
TimzOwen <timzowen@gmail.com>
2024-07-22 11:04:27 +0300

    category details impl

--- android_prac_apps/recipe_App/app/src/main/java/com/timzowen/recipeapp/CategoryDetailScreen.kt
+++ android_prac_apps/recipe_App/app/src/main/java/com/timzowen/recipeapp/CategoryDetailScreen.kt
@@ -1,8 +1,13 @@
 package com.timzowen.recipeapp
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -10,12 +15,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 
 @Composable
 fun CategoryDetailScreen(category: Category){
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp),
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally) {
+
         Text(text = category.strCategory, textAlign = TextAlign.Center)
+        Image(painter = rememberAsyncImagePainter(category.strCategoryThumb),
+            contentDescription = "${category.strCategory} Thumbnail",
+            modifier = Modifier
+                .wrapContentSize()
+                .aspectRatio(1f))
+        Text(text = category.strCategoryDescription,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.verticalScroll(rememberScrollState()))
     }
 }
 
@@ -26,6 +43,9 @@ fun CategoryDetailScreenPreview(){
         "1",
         "Fast Food",
         "https:",
-        "More food"
-    ))
+        "More food More food More food is sweet food" +
+                "More food More food More food More food More food More food" +
+                " More food More food More food More food More food More food" +
+                "More food More food More food")
+    )
 }
