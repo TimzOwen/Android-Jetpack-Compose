3dea9709845b3b695adeb7ca7241a401280c90dc
TimzOwen <timzowen@gmail.com>
2024-08-02 05:33:21 +0300

    mobile notification

--- android_team/kotlin_language/classes_objects/SmartDevice.kt
+++ android_team/kotlin_language/classes_objects/SmartDevice.kt
@@ -1023,3 +1023,24 @@ val treat: () -> Unit = {
 
 //
 //
+// Classes and Objects {Practice Quiz}
+
+// MOBILE NOTIFICATION
+fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
+    
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    if(numberOfMessages < 100){
+        println("You have $numberOfMessages notifications.")
+    }else if(numberOfMessages > 100){
+        println("Your phone is blowing up! You have 99+ notifications.")
+    }
+}
+
+
+//
