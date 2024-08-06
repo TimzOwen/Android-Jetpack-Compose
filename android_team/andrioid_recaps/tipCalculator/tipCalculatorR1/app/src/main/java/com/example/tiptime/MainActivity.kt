839be67d685a9f851a679af9a6ac41d009547659
TimzOwen <timzowen@gmail.com>
2024-08-06 11:59:10 +0300

    recap for state recomposition

--- android_team/andrioid_recaps/tipCalculator/tipCalculatorR1/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ android_team/andrioid_recaps/tipCalculator/tipCalculatorR1/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -34,7 +34,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -66,8 +65,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout() {
-    var amountInput by remember { mutableStateOf("") }
-    val amount = amountInput.toDoubleOrNull()?:0.0
+    var amountTip by remember { mutableStateOf("") }
+    val amount = amountTip.toDoubleOrNull() ?: 0.0
     val tip = calculateTip(amount)
     Column(
         modifier = Modifier
@@ -84,9 +83,10 @@ fun TipTimeLayout() {
                 .align(alignment = Alignment.Start)
         )
         EditNumberField(
-            value = amountInput,
-            onValueChange = {amountInput = it},
-            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+            value = amountTip,
+            onValueChange = {amountTip = it},
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
+        )
         Text(
             text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
@@ -96,19 +96,18 @@ fun TipTimeLayout() {
 }
 
 @Composable
-fun EditNumberField(value: String,
-                    onValueChange: (String) -> Unit,modifier:
-                    Modifier = Modifier
-) {
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier){
     TextField(
         value = value,
-        onValueChange =onValueChange,
-        singleLine = true,
+        onValueChange = onValueChange,
         label = { Text(text = stringResource(id = R.string.bill_amount))},
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        modifier=modifier)
+        singleLine = true,
+        modifier = modifier)
 }
-
 
 private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
     val tip = tipPercent / 100 * amount
