15436c57fdd192beba1721628f7964851bc9e064
TimzOwen <timzowen@gmail.com>
2024-08-08 17:31:40 +0300

    art card

--- android_team/android_prac_apps/artSpace/app/src/main/java/com/timzowen/artspace/MainActivity.kt
+++ android_team/android_prac_apps/artSpace/app/src/main/java/com/timzowen/artspace/MainActivity.kt
@@ -4,22 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.timzowen.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,12 +51,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ArtSpaceApp() {
-    ArtWithContents(artPicture = , modifier = )
+fun ArtSpaceApp(modifier: Modifier = Modifier) {
+    ArtWithContents(
+        artTitle = "The Lemon tree",
+        artDescription = "New juice in town named juice!",
+        artPicture = R.drawable.lemon_drink,
+        modifier = modifier)
 }
 
 @Composable
-fun ArtWithContents(artPicture: Int, modifier: Modifier){
+fun ArtWithContents(artPicture: Int, modifier: Modifier, artTitle: String, artDescription: String){
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -51,16 +69,37 @@ fun ArtWithContents(artPicture: Int, modifier: Modifier){
         Image(
             painter = painterResource(id = artPicture),
             contentDescription = null,
-            modifier=modifier)
+            modifier= Modifier
+                .padding(20.dp)
+                .height(300.dp)
+                .width(300.dp)
+                .border(border = BorderStroke(2.dp, color = Color.Gray), shape = RectangleShape)
+                .shadow(elevation = 4.dp)
+                .padding(10.dp)
+        )
 
-        Column {
-            Text(text ="Art title desc" )
+        Card(
+            modifier = Modifier.padding(horizontal = 45.dp, vertical = 90.dp)
+        ) {}
+
+        Column (modifier = Modifier.padding(16.dp)){
+            Text(
+                text ="Art title desc",
+                fontWeight = FontWeight.Bold
+            )
             Text(text = "Art Desc")
         }
-        Box(modifier = modifier){
+        Box(modifier = Modifier){
             Row {
-                Button(onClick = { /*TODO*/ }) {}
-                Button(onClick = { /*TODO*/ }) {}
+                Button(
+                    onClick =  {},
+                    modifier = Modifier) { Text(text = "Next")}
+
+                Spacer(modifier = Modifier.size(16.dp))
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier) { Text(text = "previous")}
             }
         }
     }
