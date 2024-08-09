a15217b67ef0270c00dccaccdf05fe54cc70031c
TimzOwen <timzowen@gmail.com>
2024-08-09 16:35:18 +0300

    extension functions

--- android_team/kotlin_language/Generic OOP/Generic.kt
+++ android_team/kotlin_language/Generic OOP/Generic.kt
@@ -182,6 +182,46 @@ fun main() {
 
 
 
+//
+//
+// Add an extension property
+// You can define a singleton object inside another class using a companion object. 
+data class Questions<T>(val questionText: String,val answer: T,
+    val diffculty: Difficulty)  
+
+
+enum class Difficulty{
+    EASY, MEDIUM, HARD
+}
+
+class Quiz{
+    val q1 = Questions("Quesition one here", "answer", Difficulty.EASY)
+    val q2 = Questions("New question",false,Difficulty.MEDIUM)
+    val q3 = Questions("Another",2,Difficulty.HARD)
+    
+    companion object StudentProgress{
+    	var total: Int =10
+    	var answered: Int = 3 
+        
+        fun Quiz.StudentProgress.printProgressBar() {
+            repeat(Quiz.answered) { print("▓") }
+            repeat(Quiz.total - Quiz.answered) { print("▒") }
+            println()
+            println(Quiz.progressText)
+}
+	}
+}
+
+  val Quiz.StudentProgress.progressText: String
+    get() = "${Quiz.answered} of ${Quiz.total} answered"
+
+
+
+
+fun main() {    
+    Quiz.printProgressBar()
+}
+
 
 
 
