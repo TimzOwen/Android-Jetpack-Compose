bba0d9478bb70cae378044397bab0b58e7bca8b6
TimzOwen <timzowen@gmail.com>
2024-08-12 02:56:13 +0300

    for each

--- android_team/kotlin_language/collections/collections.kt
+++ android_team/kotlin_language/collections/collections.kt
@@ -124,3 +124,81 @@ fun main() {
     println(solarSystem.size)  // check no. of elements
     
 }
+
+
+
+//
+//
+//
+// forEach() and string templates with lambdas
+
+class Cookie(
+    val name: String,
+    val softBaked: Boolean,
+    val hasFilling: Boolean,
+    val price: Double
+)
+
+val cookies = listOf(
+    Cookie(
+        name = "Chocolate Chip",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.69
+    ),
+    Cookie(
+        name = "Banana Walnut", 
+        softBaked = true, 
+        hasFilling = false, 
+        price = 1.49
+    ),
+    Cookie(
+        name = "Vanilla Creme",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.59
+    ),
+    Cookie(
+        name = "Chocolate Peanut Butter",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.49
+    ),
+    Cookie(
+        name = "Snickerdoodle",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.39
+    ),
+    Cookie(
+        name = "Blueberry Tart",
+        softBaked = true,
+        hasFilling = true,
+        price = 1.79
+    ),
+    Cookie(
+        name = "Sugar and Sprinkles",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.39
+    )
+)
+
+fun main() {
+    
+    // use for each to loop thru items 
+    cookies.forEach(){
+        println("Menu item: ${it.name}")
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
