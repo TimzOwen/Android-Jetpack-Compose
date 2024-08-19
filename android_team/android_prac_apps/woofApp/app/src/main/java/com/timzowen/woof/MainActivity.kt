d0e6d29a13d6c3f6868f006a6a3ceb8e316affe2
TimzOwen <timzowen@gmail.com>
2024-08-19 18:52:33 +0300

    dropwon animation

--- android_team/android_prac_apps/woofApp/app/src/main/java/com/timzowen/woof/MainActivity.kt
+++ android_team/android_prac_apps/woofApp/app/src/main/java/com/timzowen/woof/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -84,7 +87,13 @@ fun WoofApp() {
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
     var expanded by remember { mutableStateOf(false) }
    Card(modifier = modifier){
-       Column {
+       Column(modifier = Modifier
+           .animateContentSize(
+               animationSpec = spring(
+                   dampingRatio = Spring.DampingRatioNoBouncy,
+                   stiffness = Spring.StiffnessMedium
+               )
+           )) {
            Row(
                modifier = modifier
                    .fillMaxWidth()
@@ -127,7 +136,6 @@ fun DogHobby(
         )
     }
 }
-
 
 @Composable
 fun DogIcon(
