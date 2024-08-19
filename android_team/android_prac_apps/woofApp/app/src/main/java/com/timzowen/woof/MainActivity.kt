874d30f1732f222eadf4c50dd111efec5650636a
TimzOwen <timzowen@gmail.com>
2024-08-19 17:47:16 +0300

    floafing action btn animation

--- android_team/android_prac_apps/woofApp/app/src/main/java/com/timzowen/woof/MainActivity.kt
+++ android_team/android_prac_apps/woofApp/app/src/main/java/com/timzowen/woof/MainActivity.kt
@@ -10,18 +10,30 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -32,6 +44,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.timzowen.woof.data.Dog
 import com.timzowen.woof.data.dogs
 import com.timzowen.woof.ui.theme.WoofTheme
+import java.nio.channels.FileChannel.MapMode
 
 
 class MainActivity : ComponentActivity() {
@@ -52,7 +65,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WoofApp() {
-    Scaffold { it ->
+    Scaffold(
+        topBar = {WoofTopAppBar()}
+    ) { it ->
         LazyColumn(contentPadding = it) {
             items(dogs) {
                 DogItem(
@@ -66,6 +81,7 @@ fun WoofApp() {
 
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
    Card(modifier = modifier){
        Row(
            modifier = modifier
@@ -74,6 +90,10 @@ fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
        ) {
            DogIcon(dog.imageResourceId)
            DogInformation(dog.name, dog.age)
+           Spacer(modifier = Modifier.weight(1f))
+           DogItemButton(
+               expanded = expanded,
+               onClick = { /*TODO*/ })
        }
    }
 }
@@ -95,6 +115,24 @@ fun DogIcon(
     )
 }
 
+@Composable
+fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = Icons.Filled.ExpandMore,
+            contentDescription = stringResource(id = R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary)
+    }
+
+
+}
 
 @Composable
 fun DogInformation(
@@ -115,18 +153,36 @@ fun DogInformation(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WoofTopAppBar(modifier: Modifier=Modifier){
-
+    CenterAlignedTopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically){
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.image_size))
+                        .padding(dimensionResource(id = R.dimen.padding_small)),
+                    painter = painterResource(id = R.drawable.ic_woof_logo),
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        modifier = modifier
+    )
 }
 
-@Preview
-@Composable
-fun WoofDarkThemePreview(){
-    WoofTheme(darkTheme = true) {
-        WoofApp()
-    }
-}
+//@Preview
+//@Composable
+//fun WoofDarkThemePreview(){
+//    WoofTheme(darkTheme = true) {
+//        WoofApp()
+//    }
+//}
 
 @Preview
 @Composable
