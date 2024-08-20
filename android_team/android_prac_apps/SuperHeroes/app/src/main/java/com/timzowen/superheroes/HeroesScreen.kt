1d2aeffc0e39189b01c1f4314c62cb230133226a
TimzOwen <timzowen@gmail.com>
2024-08-20 15:35:25 +0300

    main activity loading lazy colmun

--- android_team/android_prac_apps/SuperHeroes/app/src/main/java/com/timzowen/superheroes/HeroesScreen.kt
+++ android_team/android_prac_apps/SuperHeroes/app/src/main/java/com/timzowen/superheroes/HeroesScreen.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.timzowen.superheroes.model.HeroesRepository.heroes
 import com.timzowen.superheroes.model.SuperHeroes
 
 
