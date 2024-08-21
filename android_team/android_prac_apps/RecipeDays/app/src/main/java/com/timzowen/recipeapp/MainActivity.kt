5c6b0876a7a78d4a987a2cd400990c3d8e4465d3
TimzOwen <timzowen@gmail.com>
2024-08-21 15:54:39 +0300

    scafold big fix

--- android_team/android_prac_apps/RecipeDays/app/src/main/java/com/timzowen/recipeapp/MainActivity.kt
+++ android_team/android_prac_apps/RecipeDays/app/src/main/java/com/timzowen/recipeapp/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.timzowen.recipeapp.screens.RecipeList
 import com.timzowen.recipeapp.ui.theme.RecipeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,22 +25,36 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                RecipeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun RecipeApp() {
+    Scaffold(
+        topBar = {
+            RecipeTopAppBar()
+        }
+    ) { innerPadding ->
+        RecipeList(paddingValues = innerPadding)
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RecipeTopAppBar(modifier: Modifier = Modifier){
+    CenterAlignedTopAppBar(
+        title = {
+            Row {
+                Text(
+                    text = "RecipeApp"
+                )
+            }
+        },
+        modifier = modifier.height(32.dp)
     )
 }
 
@@ -42,6 +62,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     RecipeAppTheme {
-        Greeting("Android")
+        RecipeApp()
     }
 }
