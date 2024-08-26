48164e427442bb2d4046407ce96586a492276743
TimzOwen <timzowen@gmail.com>
2024-08-26 23:28:16 +0300

    code base refactor

--- android_prac_apps/mvvm/JetNotesApp/app/src/main/java/com/timzowen/jetnotesapp/MainActivity.kt
+++ android_prac_apps/mvvm/JetNotesApp/app/src/main/java/com/timzowen/jetnotesapp/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.timzowen.jetnotesapp.screen.NoteScreen
 import com.timzowen.jetnotesapp.ui.theme.JetNotesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetNotesAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    NoteScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetNotesAppTheme {
-        Greeting("Android")
+        NoteScreen()
     }
 }
--- android_prac_apps/mvvm/JetNotesApp/app/src/main/java/com/timzowen/jetnotesapp/screen/NoteScreen.kt
+++ android_prac_apps/mvvm/JetNotesApp/app/src/main/java/com/timzowen/jetnotesapp/screen/NoteScreen.kt
@@ -1,15 +1,87 @@
 package com.timzowen.jetnotesapp.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.timzowen.jetnotesapp.R
+import com.timzowen.jetnotesapp.components.NoteButton
+import com.timzowen.jetnotesapp.components.NoteInputText
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NoteScreen(){
+fun NoteScreen() {
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+        Column {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                actions = {
+                    Icon(
+                        imageVector = Icons.Rounded.Notifications,
+                        contentDescription = "Icon"
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color(0xFFDADFE3))
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                NoteInputText(
+                    modifier = Modifier.padding(top = 9.dp, bottom = 9.dp),
+                    text = title,
+                    label = "Title",
+                    onTextChange = {
+                        if (it.all { char ->
+                                char.isLetter() || char.isWhitespace()
+                            })
+                            title = it
+                    }
+                )
+                NoteInputText(
+                    modifier = Modifier.padding(top = 9.dp, bottom = 9.dp),
+                    text = description,
+                    label = "Add a note",
+                    onTextChange = {
+                        if (it.all { char ->
+                                char.isLetter() || char.isWhitespace()
+                            })
+                            description = it
+                    }
+                )
+                NoteButton(
+                    text = "Save",
+                    onClick = { }
+                )
+            }
+        }
+    }
 
-}
 
 @Composable
 @Preview(showBackground = true)
-fun NoteScreenPreview(){
+fun NoteScreenPreview() {
     NoteScreen()
 }
