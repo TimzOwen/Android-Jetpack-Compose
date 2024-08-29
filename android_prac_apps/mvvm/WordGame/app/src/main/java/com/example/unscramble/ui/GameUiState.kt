e14983820ee35d4d3be2266c318cf8950f04c872
TimzOwen <timzowen@gmail.com>
2024-08-29 10:27:36 +0300

    mvvm on desert app

--- android_prac_apps/mvvm/WordGame/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ android_prac_apps/mvvm/WordGame/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -6,5 +6,6 @@ data class GameUiState(
     val currentScrambledWord: String = "",
     val currentWordCount: Int = 1,
     val score: Int = 0,
-    val isGuessedWordWrong: Boolean = false
+    val isGuessedWordWrong: Boolean = false,
+    val isGameOver: Boolean = false
 )
