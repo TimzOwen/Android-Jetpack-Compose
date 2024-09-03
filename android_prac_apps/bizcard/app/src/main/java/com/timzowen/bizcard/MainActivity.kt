4a62e543b5ae7910d13c5ab3cf6ff83b2bf17cf3
TimzOwen <timzowen@gmail.com>
2024-09-03 19:21:28 +0300

    surface image card

--- android_prac_apps/bizcard/app/src/main/java/com/timzowen/bizcard/MainActivity.kt
+++ android_prac_apps/bizcard/app/src/main/java/com/timzowen/bizcard/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.timzowen.bizcard.ui.theme.BizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +33,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BizCardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    BizCard()
                 }
             }
         }
@@ -31,17 +42,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BizCard() {
+    Surface(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()) {
+        Card(modifier = Modifier
+            .height(300.dp)
+            .width(200.dp)
+            .padding(16.dp),
+            elevation = 4.dp,
+            shape = RoundedCornerShape(corner = CornerSize(15.dp))
+        ){
+            Surface(
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(5.dp),
+                shape = CircleShape,
+                tonalElevation = 4.dp,
+                color = MaterialTheme.colorScheme.surface.copy(alpha = 0.5f),
+                border = BorderStroke(0.5.dp, color = Color.LightGray)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.profile_image),
+                    contentDescription = "profile image",
+                    modifier = Modifier.size(135.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BizCardTheme {
-        Greeting("Android")
+        BizCard()
     }
 }
