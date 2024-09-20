1dbb73adaa8104bd074fc63afee1fb8408f27971
= <=>
2024-09-20 15:03:00 +0300

    classes instantiation

--- android_team/kotlin_language/classes_objects/Car.kt
+++ android_team/kotlin_language/classes_objects/Car.kt
@@ -53,7 +53,28 @@ class Car{
 //
 //
 //
-// constructors arguements
+// default arguements 
+fun main() {
+    val myCar = Car()
+    myCar.color = "white"
+    myCar.model = "Subaru"
+    println("car color ${myCar.color} model type ${myCar.model}")
+}
+
+class Car(color: String = "white", model: String = "Subaru") {
+    var color: String = color
+    var model: String = model
+
+    fun drive(){
+        println("Driving south....")
+    }
+}
+
+
+//
+//
+//
+// constructors parameters
 fun main() {
     val myCar = Car()
     myCar.color = "white"
@@ -72,7 +93,7 @@ class Car(color: String = "Red", var model: String = "Honda") {
 //
 //
 //
-// overwite default arguements
+// overwite default parameters
 fun main() {
     val myCar = Car()
     myCar.color = "white"
@@ -92,3 +113,60 @@ class Car(color: String = "Red", var model: String = "Honda") {
 //
 //
 //
+// default args when instanting the object class
+fun main() {
+    val myCar = Car(color = "yellow", model = "honda")
+    println("car color ${myCar.color} model type ${myCar.model}")
+}
+
+class Car(var color: String = "white", var model: String = "Subaru") {
+    
+    fun drive(){
+        println("Driving south....")
+    }
+}
+
+//
+//
+// 
+// Init block -> always executed once
+fun main() {
+    val myCar = Car(color = "yellow", model = "honda")
+    println("car color ${myCar.color} model type ${myCar.model}")
+}
+
+class Car(var color: String = "white", var model: String = "Subaru") {
+
+    init {
+        color = "maroon"
+        model = "Gwagon"
+    }
+    fun drive(){
+        print("Driving south....")
+    }
+}
+
+
+//
+//
+//
+// Multiple functions in class
+fun main() {
+    val myCar = Car(color = "yellow", model = "honda")
+    myCar.maxSpeed(minSpeed = 80.0, maxSpeed = 205.9)
+    println("car color ${myCar.color} model type ${myCar.model}")
+}
+
+class Car(var color: String = "white", var model: String = "Subaru") {
+
+    init {
+        color = "maroon"
+        model = "Gwagon"
+    }
+    fun drive(){
+        print("Driving south....")
+    }
+    fun maxSpeed(minSpeed: Double = 50.0, maxSpeed: Double=199.0){
+        println("Current max speed $maxSpeed m/s and min speed $minSpeed")
+    }
+}
