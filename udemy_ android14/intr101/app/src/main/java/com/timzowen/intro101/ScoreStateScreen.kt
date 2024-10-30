94615e5f652fe599adf604309df7003be3059301
= <=>
2024-10-30 13:56:05 +0300

    updated view model

--- udemy_ android14/intr101/app/src/main/java/com/timzowen/intro101/ScoreStateScreen.kt
+++ udemy_ android14/intr101/app/src/main/java/com/timzowen/intro101/ScoreStateScreen.kt
@@ -5,41 +5,72 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-//@Preview(showBackground = true)
-@Composable
-fun ScoreScreenState(viewModel: StateScoreViewModel){
-    val currentScore by viewModel.score.observeAsState(initial = "0")
-    val totalScore by viewModel.totalScore.observeAsState(initial = "100")
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+@Composable
+fun TeamScore(viewModel: StateScoreViewModel){
+    val currentScore by viewModel.currentScore.observeAsState(initial = "0")
+    var finalScore by remember { mutableStateOf("0") }
+
+
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
         Text(
-            text = "Score: $currentScore of $totalScore",
+            text = "Score: $currentScore of $finalScore",
             style = TextStyle(fontSize = 22.sp, fontWeight = FontWeight.Bold))
-        OutlinedTextField(
-            value = currentScore,
-            onValueChange ={ viewModel.onScoreUpdate(it) },
-            label = { Text(text = "Enter Score")})
         Spacer(modifier = Modifier.height(10.dp))
-        OutlinedTextField(
-            value = totalScore,
-            onValueChange ={viewModel.totalScoreUpdate(it)},
-            label = { Text(text = "Total Score")})
+        OutlinedTextScore(
+            value = currentScore,
+            onValueChange = {viewModel.updateScore(it)},
+            labelText = { Text(
+                text = "score board",
+                style = TextStyle(fontSize = 16.sp))
+            }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        OutlinedTextScore(
+            value = finalScore,
+            onValueChange = {finalScore = it},
+            labelText = { Text(
+                    text = "score board",
+                    style = TextStyle(fontSize = 16.sp))
+            }
+        )
     }
 }
 
+@Composable
+fun OutlinedTextScore(value: String, onValueChange: (String) -> Unit,
+                      labelText: @Composable () -> Unit){
+    OutlinedTextField(
+        value = value ,
+        onValueChange = onValueChange,
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+        label = labelText)
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun TeamScorePreview(){
+//    TeamScore()
+//}
--- udemy_ android14/intr101/app/src/main/java/com/timzowen/intro101/StateScoreViewModel.kt
+++ udemy_ android14/intr101/app/src/main/java/com/timzowen/intro101/StateScoreViewModel.kt
@@ -4,18 +4,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class StateScoreViewModel: ViewModel() {
+class StateScoreViewModel : ViewModel() {
+    private val _currentScore = MutableLiveData<String>()
+    var currentScore: LiveData<String> = _currentScore
 
-    private val _score = MutableLiveData<String>()
-    val score: LiveData<String> = _score
     private val _totalScore = MutableLiveData<String>()
     val totalScore: LiveData<String> = _totalScore
 
-    fun onScoreUpdate(newScore: String){
-        _score.value = newScore
+    fun updateScore(newScore: String){
+        _currentScore.value = newScore
     }
 
-    fun totalScoreUpdate(finalScore: String){
-        _totalScore.value = finalScore
+    fun updateTotalScore(totalScore: String){
+        _totalScore.value = totalScore
     }
 }
