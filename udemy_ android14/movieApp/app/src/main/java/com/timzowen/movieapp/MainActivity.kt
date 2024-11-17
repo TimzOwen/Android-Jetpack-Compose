a69f161dd9be86a4948d06536be14f36a290da40
= <=>
2024-11-17 17:41:33 +0300

    lazy Column and cards

--- udemy_ android14/movieApp/app/src/main/java/com/timzowen/movieapp/MainActivity.kt
+++ udemy_ android14/movieApp/app/src/main/java/com/timzowen/movieapp/MainActivity.kt
@@ -5,12 +5,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.AlertDialogDefaults.containerColor
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -21,10 +37,14 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -55,16 +75,6 @@ fun MyApp(content: @Composable () -> Unit) {
                         titleContentColor = MaterialTheme.colorScheme.primary
                     ),
                     title = { Text(text = "Movies") })
-            },
-            bottomBar = {
-                BottomAppBar(
-                    containerColor = Color.Magenta,
-                    contentColor = MaterialTheme.colorScheme.primary
-                ) {
-                    Text(text = "Home",
-                        style = TextStyle(fontSize = 16.sp)
-                    )
-                }
             }
         ) { innerPadding ->
             Box(modifier = Modifier.padding(innerPadding)) {
@@ -75,10 +85,49 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MainContent() {
-    Surface(color = MaterialTheme.colorScheme.background) {
+fun MainContent(
+    moviesList: List<String> = listOf(
+        "One Master",
+        "Team AG",
+        "Team Blue",
+        "SWAT",
+        "500"
+    )
+) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        LazyColumn {
+            items(items = moviesList) {
+                MovieCard(movie = it)
+            }
+        }
     }
-    Text(text = "Hello")
+}
+
+
+@Composable
+fun MovieCard(movie: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(6.dp)
+            .height(130.dp),
+        elevation = CardDefaults.cardElevation(6.dp),
+        colors = CardDefaults.cardColors(Color.White),
+        shape = RoundedCornerShape(corner = CornerSize(12.dp))
+    ) {
+        Row(horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                tonalElevation = 4.dp) {
+                Icon(imageVector = Icons.Default. AccountBox, contentDescription = null)
+            }
+            Text(text = movie)
+        }
+    }
 }
 
 @Preview(showBackground = true)
