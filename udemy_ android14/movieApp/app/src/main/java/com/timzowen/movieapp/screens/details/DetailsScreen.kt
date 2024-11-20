7a0236dea897a554a98e0d165708f2419611348d
= <=>
2024-11-20 17:17:30 +0300

    navigation graphs

--- udemy_ android14/movieApp/app/src/main/java/com/timzowen/movieapp/screens/details/DetailsScreen.kt
+++ udemy_ android14/movieApp/app/src/main/java/com/timzowen/movieapp/screens/details/DetailsScreen.kt
@@ -1,34 +1,83 @@
 package com.timzowen.movieapp.screens.details
 
+import android.widget.Space
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailsScreen(navController: NavController, movieData: String?) {
-    Surface(
-        modifier = Modifier
-            .padding(32.dp)
-            .fillMaxSize()
-    ) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
-            Text(
-                text = "Details screen: ${movieData.toString()}",
-                style = MaterialTheme.typography.labelLarge
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.shadow(elevation = 5.dp),
+                colors = topAppBarColors(
+                    containerColor = Color.Magenta,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                title = {
+                    Row(horizontalArrangement = Arrangement.Start) {
+                        Icon(
+                            modifier = Modifier.clickable {
+                                navController.popBackStack()
+                            },
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "back" )
+                        Spacer(modifier = Modifier.width(100.dp))
+                        Text(text = "Movie Details")
+                    }
+                }
             )
+        }
+    )  { innerPadding ->
+        Surface(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Details screen: ${movieData.toString()}",
+                    style = MaterialTheme.typography.labelLarge
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Button(onClick = { navController.popBackStack() }) {
+                    Text(text = "Home")
+                }
+            }
         }
     }
 }
+
