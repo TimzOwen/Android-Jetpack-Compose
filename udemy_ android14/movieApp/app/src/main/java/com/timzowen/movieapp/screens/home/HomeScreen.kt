28d58d67e164ca92e044627e1e449ff5f40af761
= <=>
2024-11-22 16:12:52 +0300

    details movie screen

--- udemy_ android14/movieApp/app/src/main/java/com/timzowen/movieapp/screens/home/HomeScreen.kt
+++ udemy_ android14/movieApp/app/src/main/java/com/timzowen/movieapp/screens/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.timzowen.movieapp.screens.home
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -15,7 +14,6 @@ import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.timzowen.movieapp.model.Movie
