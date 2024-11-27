9be32de69fdf4bef726c3ce1390adac24bb82fe1
= <=>
2024-11-27 10:31:33 +0300

    components

--- Recaps/udemy/NoteAppMVVM/app/src/main/java/com/timzowen/noteappmvvm/screen/NoteScreen.kt
+++ Recaps/udemy/NoteAppMVVM/app/src/main/java/com/timzowen/noteappmvvm/screen/NoteScreen.kt
@@ -1,36 +1,62 @@
 package com.timzowen.noteappmvvm.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.timzowen.noteappmvvm.R
+import com.timzowen.noteappmvvm.components.NoteInputText
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NoteScreen(){
-    Scaffold(topBar = {
+fun NoteScreen() {
+    Column(
+        modifier = Modifier
+            .padding(6.dp)
+            .fillMaxSize()
+    ) {
         TopAppBar(
-            title = { Text(text = "JetNotesApp") },
-            colors = TopAppBarDefaults.topAppBarColors(Color.Gray),
-            modifier = Modifier.padding(3.dp)
+            title = { Text(text = stringResource(id = R.string.app_name)) },
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = Color(0xFFDADFE3)),
+            actions = {
+                Icon(
+                    imageVector = Icons.Default.Notifications,
+                    contentDescription = stringResource(R.string.notification)
+                )
+            },
         )
-    }) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-
+        Spacer(modifier = Modifier.height(6.dp))
+        Column(modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            NoteInputText(
+                label = "Title",
+                text = "data",
+                onTextChange = {})
         }
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
-fun NoteScreenPreview(){
+fun NoteScreenPreview() {
     NoteScreen()
 }
