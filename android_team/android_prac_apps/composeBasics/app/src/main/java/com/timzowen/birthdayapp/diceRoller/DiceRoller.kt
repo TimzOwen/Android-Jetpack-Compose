35827e0639f27aacbb3fb2a48da23b3770292b92
= <=>
2025-02-15 16:00:58 +0300

    dice

--- android_team/android_prac_apps/composeBasics/app/src/main/java/com/timzowen/birthdayapp/diceRoller/DiceRoller.kt
+++ android_team/android_prac_apps/composeBasics/app/src/main/java/com/timzowen/birthdayapp/diceRoller/DiceRoller.kt
@@ -9,6 +9,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -24,17 +28,28 @@ fun DiceRollerApp(){
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when(result){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column (modifier = modifier
         .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally){
         
         Image(
-            painter = painterResource(R.drawable.dice_1),
-            contentDescription = "dice 1"
+            painter = painterResource(imageResource),
+            contentDescription = result.toString()
         )
         Spacer(Modifier.height(4.dp))
-        Button(onClick = {})  {
+        Button(onClick = {
+            result = (1..6).random()
+        })  {
             Text(stringResource(R.string.roll))
         }
     }
