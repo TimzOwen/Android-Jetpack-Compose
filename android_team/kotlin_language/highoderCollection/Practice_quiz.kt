039db99fff937a498c9c20429f1beff478112d01
= <=>
2025-02-28 11:34:52 +0300

    extention functions

--- android_team/kotlin_language/highoderCollection/Practice_quiz.kt
+++ android_team/kotlin_language/highoderCollection/Practice_quiz.kt
@@ -107,4 +107,100 @@ println("you have ${shortEvents.size} events events")
 /////
 ///
 //
+// Task 5
+data class Event(
+	val title: String = "Study. Kotlin",
+    val description: String? = "Commit to studying Kotlin at least 15 minutes per day.",
+    val daypart: Daypart,
+    val durationInMinutes: Int = 15
+)
+
+enum class Daypart{
+    MORNING,
+    AFTERNOON,
+    EVENING
+}
+fun main() {
+val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+
+val events = mutableListOf(event1,event2,event3,event4,event5,event6)
+val groupedEvents = events.groupBy{it.daypart}
+groupedEvents.forEach{(daypart,events) ->
+    println("$daypart: ${events.size} events")
+	}
+}
+
+
+///////
+/////
+///
 //
+// Task 6. simplify --> checking last item in the list
+data class Event(
+	val title: String = "Study. Kotlin",
+    val description: String? = "Commit to studying Kotlin at least 15 minutes per day.",
+    val daypart: Daypart,
+    val durationInMinutes: Int = 15
+)
+
+enum class Daypart{
+    MORNING,
+    AFTERNOON,
+    EVENING
+}
+fun main() {
+val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+
+val events = mutableListOf(event1,event2,event3,event4,event5,event6)
+println("Last event of the day: ${events[events.size - 1].title}")
+println("last event of the day: ${events.last().daypart}")
+
+}
+
+////////
+//////
+////
+//
+// Task 7 --> Extension function 
+
+data class Event(
+	val title: String = "Study. Kotlin",
+    val description: String? = "Commit to studying Kotlin at least 15 minutes per day.",
+    val daypart: Daypart,
+    val durationInMinutes: Int = 15
+)
+
+enum class Daypart{
+    MORNING,
+    AFTERNOON,
+    EVENING
+}
+fun main() {
+val Event.durationOfEvent: String
+    get() = if (this.durationInMinutes < 60) {
+        "short"
+    } else {
+        "long"
+    }
+val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+
+val events = mutableListOf(event1,event2,event3,event4,event5,event6)
+
+println("Duration of first event of the day: ${events[0].durationOfEvent}")
+
+}
