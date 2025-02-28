c724cd4ab8cd8468d1a09473a248636dbfc8f457
= <=>
2025-02-28 14:37:23 +0300

    affirmations lazyColumn

--- kotlin programming/Kotlin_Compose/CodeRunner.kt
+++ kotlin programming/Kotlin_Compose/CodeRunner.kt
@@ -1,6 +1,33 @@
-internal object Test {
-    @kotlin.jvm.JvmStatic
-    fun main(args: Array<String>) {
-        println("My First Java Program.")
+data class Event(
+    val title: String = "Study Kotlin",
+    val description: String? = "Commit to studying Kotlin at least 15 minutes per day.",
+    val daypart: Daypart,
+    val durationInMinutes: Int = 15
+)
+
+enum class Daypart {
+    MORNING,
+    AFTERNOON,
+    EVENING
+}
+
+// Extension property - must be outside main()
+val Event.durationOfEvent: String
+    get() = if (this.durationInMinutes < 60) {
+        "short"
+    } else {
+        "long"
     }
+
+fun main() {
+    val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+    val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+    val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+    val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+    val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+    val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+
+    val events = mutableListOf(event1, event2, event3, event4, event5, event6)
+
+    println("Duration of first event of the day: ${events[0].durationOfEvent}")
 }
