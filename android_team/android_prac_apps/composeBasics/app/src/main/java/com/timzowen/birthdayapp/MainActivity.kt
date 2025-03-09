94e8506deb19e39d3387a37eff60e76475b0f4c6
= <=>
2025-03-09 07:32:30 +0300

    Material theming

--- android_team/android_prac_apps/composeBasics/app/src/main/java/com/timzowen/birthdayapp/MainActivity.kt
+++ android_team/android_prac_apps/composeBasics/app/src/main/java/com/timzowen/birthdayapp/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.timzowen.birthdayapp.affirmationsApp.AffirmationsApp
 import com.timzowen.birthdayapp.artSpace.ArtSpaceApp
@@ -20,20 +22,23 @@ import com.timzowen.birthdayapp.phonecontact.PhoneContact
 import com.timzowen.birthdayapp.quadrant2.QuadrantApp
 import com.timzowen.birthdayapp.tipCalculator.TipCalculator
 import com.timzowen.birthdayapp.ui.theme.BirthdayAppTheme
+import com.timzowen.birthdayapp.ui.theme.WoofTheme
+import com.timzowen.birthdayapp.woofApp.WoofApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BirthdayAppTheme {
+            WoofTheme {
                 Surface(
                     modifier = Modifier
                         .fillMaxSize()
-                        .systemBarsPadding(),
-                    color = MaterialTheme.colorScheme.background
+                        .systemBarsPadding()
+//                    color = MaterialTheme.colorScheme.background
                 ) {
-                    MainCourse()
+                    WoofApp()
+//                    MainCourse()
 //                    AffirmationsApp()
 //                    ArtSpaceApp()
 //                    TipCalculator()
@@ -51,3 +56,10 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Preview(showBackground = true)
+@Composable
+fun WoofPreview() {
+    WoofTheme(darkTheme = true) {
+        WoofApp()
+    }
+}
---
+++ android_team/android_prac_apps/composeBasics/app/src/main/java/com/timzowen/birthdayapp/woofApp/WoofApp.kt
@@ -0,0 +1,11 @@
+package com.timzowen.birthdayapp.woofApp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WoofApp(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.fillMaxSize()) {  }
+}
