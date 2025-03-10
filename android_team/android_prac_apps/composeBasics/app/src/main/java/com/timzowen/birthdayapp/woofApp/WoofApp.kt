b147e0c6142bc00d8fc41c93346fb072f7757fd2
= <=>
2025-03-10 09:15:59 +0300

    dynamic contrast

--- android_team/android_prac_apps/composeBasics/app/src/main/java/com/timzowen/birthdayapp/woofApp/WoofApp.kt
+++ android_team/android_prac_apps/composeBasics/app/src/main/java/com/timzowen/birthdayapp/woofApp/WoofApp.kt
@@ -14,7 +14,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,15 +30,18 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.timzowen.birthdayapp.R
+import com.timzowen.birthdayapp.ui.theme.AppTheme
 import com.timzowen.birthdayapp.woofApp.dog.Dog
 import com.timzowen.birthdayapp.woofApp.dog.dogs
 
 @Composable
 fun WoofApp(modifier: Modifier = Modifier) {
-    Column(modifier.fillMaxSize()) {
-        LazyColumn {
+    Scaffold {
+        LazyColumn(contentPadding = it) {
             items(dogs){ dog ->
-                WoofAppCard(modifier,dog)
+                WoofAppCard(
+                    dog =dog,
+                    modifier = Modifier.padding(dimensionResource( R.dimen.padding_small)))
             }
         }
     }
@@ -44,11 +49,9 @@ fun WoofApp(modifier: Modifier = Modifier) {
 
 @Composable
 fun WoofAppCard(modifier: Modifier = Modifier, dog: Dog) {
-    Surface(
-        modifier.padding(dimensionResource(R.dimen.padding_small))) {
+    Card(modifier=modifier) {
         Row(modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight()
             .padding(4.dp),
             verticalAlignment = Alignment.CenterVertically) {
             Box(
@@ -76,7 +79,18 @@ fun WoofAppCard(modifier: Modifier = Modifier, dog: Dog) {
 
 @Preview(showBackground = true)
 @Composable
+fun WoofAppDarkThemePreview() {
+    val dog = Dog(R.drawable.koda, R.string.dog_name_1, 2, R.string.dog_description_1)
+    AppTheme(darkTheme = true) {
+        WoofApp()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
 fun WoofAppPreview() {
     val dog = Dog(R.drawable.koda, R.string.dog_name_1, 2, R.string.dog_description_1)
+    AppTheme(darkTheme = false) {
     WoofApp(modifier = Modifier)
+    }
 }
